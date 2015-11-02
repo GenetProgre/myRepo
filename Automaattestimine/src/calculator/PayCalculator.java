761c31831716a55e93d9c0fc8db4c1cd54abb600
Genet Schneider <genetschneider95@gmail.com>
2015-11-02 20:12:05 +0200

    Update

--- Automaattestimine/src/calculator/PayCalculator.java
+++ Automaattestimine/src/calculator/PayCalculator.java
@@ -1,21 +1,14 @@
 package calculator;
 
-//represents reusable domain service which calculates daily salaries
 public class PayCalculator {
-	// employee types
+	
 	public static final int JUNIOR = 1;
 	public static final int SENIOR = 2;
 	public static final int SPECIALIST = 3;
-
-	// my crappy screen test
-	public static void main(final String[] args) {
-		PayCalculator payCalculator = new PayCalculator();
-		System.err.println(payCalculator.pay(SPECIALIST, 3) + " should be 66");
-	}
-  
+	
 	public int payJunior(int sum, final int hours) {
-		if (hours > 8) { // if longer than eight hours
-			sum = 10 * (hours - 8) * 2; // double pay
+		if (hours > 8) {
+			sum = 10 * (hours - 8) * 2;
 			sum += 10 * 8;
 			return sum;
 		} else {
@@ -25,8 +18,8 @@ public class PayCalculator {
 	}
 
 	public int paySenior(int sum, final int hours) {
-		if (hours > 8) { // if longer than eight hours
-			sum = 15 * (hours - 8) * 2; // double pay
+		if (hours > 8) {
+			sum = 15 * (hours - 8) * 2;
 			sum += 15 * 8;
 			return sum;
 		} else {
@@ -36,8 +29,8 @@ public class PayCalculator {
 	}
 
 	public int paySpecialist(int sum, final int hours) {
-		if (hours > 9) { // if longer than nine hours
-			sum = 22 * (hours - 9) * 3; // triple pay after 9 hours
+		if (hours > 9) {
+			sum = 22 * (hours - 9) * 3;
 			sum += 22 * 9;
 			return sum;
 		} else {
